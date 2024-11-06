20762f123aaf25146e856f6326ae1ae65dbadde7
centinna-l <jerryjoy99@gmail.com>
2024-11-06 12:48:05 -0500

    Initial Commit

---
+++ Ecommerce API/Ecommerce_APIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ecommerce_APIApp.swift
+//  Ecommerce API
+//
+//  Created by Jerry Joy on 2024-11-06.
+//
+
+import SwiftUI
+
+@main
+struct Ecommerce_APIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
